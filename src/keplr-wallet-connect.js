80f6f57b109936a3950851fdfe49e8696344de86
soaryong-c <91711862+soaryong-c@users.noreply.github.com>
2022-04-09 21:30:26 +0900

    Update keplr-wallet-connect.js

--- src/keplr-wallet-connect.js
+++ src/keplr-wallet-connect.js
@@ -4,6 +4,7 @@ import WalletConnect from '@walletconnect/client';
 import { payloadId } from '@walletconnect/utils';
 
 export async function connect() {
+  //"signingMethods" run urlscheme on "Deep link".
   const connector = new WalletConnect({
     bridge: 'https://bridge.walletconnect.org',
     signingMethods: ['keplr_enable_wallet_connect_v1', 'keplr_sign_amino_wallet_connect_v1'],
